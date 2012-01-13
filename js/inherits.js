17167be73f162d1aaa7ff49bdf49fec8ba56b793
Maciej Małecki <maciej.malecki@notimplemented.org>
2012-01-13 11:30:50 +0100

    [api] Add `inherits` function stolen from node.js core

---
+++ js/inherits.js
@@ -0,0 +1,13 @@
+// shamelessly stolen from node.js core
+var inherits = function(ctor, superCtor) {
+  ctor.super_ = superCtor;
+  ctor.prototype = Object.create(superCtor.prototype, {
+    constructor: {
+      value: ctor,
+      enumerable: false,
+      writable: true,
+      configurable: true
+    }
+  });
+};
+
