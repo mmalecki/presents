7cbcdce18508af3174acbfc2088fad240ec354ca
Maciej Małecki <maciej.malecki@notimplemented.org>
2012-04-03 15:03:30 +0100

    [ux] Add vim-like go to

--- js/presents/plugins/controls.js
+++ js/presents/plugins/controls.js
@@ -1,16 +1,30 @@
 (function (exports) {
   var Controls = exports.Controls = {};
 
+  var pageNumber = null;
+
   Controls.attach = function (options) {
     var self = this;
 
     document.addEventListener('keypress', function (e) {
       var c = String.fromCharCode(e.keyCode);
-      if (c == "h" || c == "k") {
+      if (pageNumber !== null) {
+        if (c === '\r') {
+          self.goTo(+pageNumber);
+          pageNumber = null;
+        }
+        else {
+          pageNumber += c;
+        }
+      }
+      else if (c == "h" || c == "k") {
         self.previous();
       }
       else if (c == "l" || c == "j") {
         self.next();
+      }
+      else if (c === ':') {
+        pageNumber = '';
       }
     });
   };
