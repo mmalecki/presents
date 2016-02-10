6bec4fab3d6defc5c615138f0c143b1c39e80607
Maciej Małecki <me@mmalecki.com>
2016-02-10 18:28:52 +0100

    Number slides if passed the parent container

--- js/presents.js
+++ js/presents.js
@@ -1,7 +1,12 @@
 (function (exports) {
   var Presents = exports.Presents = function (options) {
+    options = options || {};
+
     var match = window.location.hash.match(/^#slide-(\d+)$/);
     this.currentSlide = match ? parseInt(match[1], 10) : 0;
+
+    this.parentContainer = options.parentContainer;
+    if (this.parentContainer) this.runHelpers();
 
     App.call(this, options);
   };
@@ -22,6 +27,13 @@
 
   Presents.prototype.previous = function () {
     this.goTo(this.currentSlide - 1);
+  };
+
+  Presents.prototype.runHelpers = function () {
+    var children = this.parentContainer.children;
+    Array.prototype.slice.call(children).forEach(function (child, i) {
+      if (!child.id) child.id = 'slide-' + i
+    })
   };
 })(window);
 
